72552628296e7e211d6e0623acfbe0da5abe44ea
esd-org-uk <support@esd.org.uk>
2019-10-14 11:51:43 +0100

    Add taxonomy mappings
    Add mappings from Bucks age group to open eligibility age group and from Bucks suitability to open eligibility disability

--- ServiceLoaderBucks/ServiceLoader/JsonMappingObjects/Result.cs
+++ ServiceLoaderBucks/ServiceLoader/JsonMappingObjects/Result.cs
@@ -175,11 +175,50 @@ namespace ServiceLoader.JsonMappingObjects
                 foreach (var ageGroup in AgeGroups)
                 {
                     yield return new Taxonomy($"age-group:{ageGroup}", ageGroup, "Bucks:age-group");
+                    switch (ageGroup.ToLowerInvariant())
+                    {
+                        case "young people":
+                            yield return new Taxonomy("OpenEligibility:20005:children", "Children", "OpenEligibility:age-group");
+                            break;
+                        case "young adults":
+                            yield return new Taxonomy("OpenEligibility:20004:teens", "Teens", "OpenEligibility:age-group");
+                            break;
+                        case "older adults":
+                            yield return new Taxonomy("OpenEligibility:20003:adults", "Adults", "OpenEligibility:age-group");
+                            yield return new Taxonomy("OpenEligibility:20006:seniors", "Seniors", "OpenEligibility:age-group");
+                            break;
+                    }
                 }
 
                 foreach (var suitability in Suitabilities)
                 {
                     yield return new Taxonomy($"suitability:{suitability}", suitability, "Bucks:suitability");
+                    switch (suitability.ToLowerInvariant())
+                    {
+                        case "autism":
+                            yield return new Taxonomy("OpenEligibility:20019:developmental-disability", "Developmental Disability", "OpenEligibility:disability");
+                            break;
+                        case "dementia":
+                        case "mental health":
+                        case "mental health/acquired brain injury":
+                            yield return new Taxonomy("OpenEligibility:20025:mental-illness", "Mental Illness", "OpenEligibility:disability");
+                            break;
+                        case "hearing or visual impairment":
+                        case "visual and/or hearing impediment":
+                            yield return new Taxonomy("OpenEligibility:20023:hearing-impairment", "Hearing Impairment", "OpenEligibility:disability");
+                            yield return new Taxonomy("OpenEligibility:20024:visual-impairment", "Visual Impairment", "OpenEligibility:disability");
+                            break;
+                        case "learning difficulties":
+                        case "learning disability":
+                            yield return new Taxonomy("OpenEligibility:20018:learning-disability", "Learning Disability", "OpenEligibility:disability");
+                            break;
+                        case "physical difficulty":
+                            yield return new Taxonomy("OpenEligibility:20022:limited-mobility", "Limited Mobility", "OpenEligibility:disability");
+                            break;
+                        case "physical disability":
+                            yield return new Taxonomy("OpenEligibility:20020:physical-disability", "Physical Disability", "OpenEligibility:disability");
+                            break;
+                    }
                 }
             }
         }
