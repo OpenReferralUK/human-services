be72cda098e4ab4b410b5f0be5b5657b9bb8ed8a
esd-org-uk <support@esd.org.uk>
2019-10-16 12:50:52 +0100

    Changes to schedule parsing
    In addition to refactoring ScheduleBuilder to make the Build method more readable, made the following changes:
    1. Don't write blank records when not enough information to write a record
    2. Only treat numbers as a time if in the format h:mm or hh:mm
    3. Fortnightly now recognised as a word meaning that not run on a weekly schedule

--- ServiceLoaderBucks/ServiceLoader/ScheduleBuilder.cs
+++ ServiceLoaderBucks/ServiceLoader/ScheduleBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using ServiceLoader.JsonMappingObjects;
 
@@ -7,6 +8,39 @@ namespace ServiceLoader
 {
     internal static class ScheduleBuilder
     {
+        private static readonly string[] ValidDays = new [] { "SU", "MO", "TU", "WE", "TH", "FR", "SA" };
+        private static readonly string[] MonthlyWords = new [] { "alternate", "1st", "2nd", "3rd", "4th", "first", "second", "third", "fourth", "last", "fortnightly" };
+        private static readonly Dictionary<string, string> WordReplacements = new Dictionary<string, string>
+        {
+            {"&", " and "},
+            {",", " and "},
+            {"mondays", "MO"},
+            {"tuesdays", "TU"},
+            {"wednesdays", "WE"},
+            {"thursdays", "TH"},
+            {"fridays", "FR"},
+            {"saturdays", "SA"},
+            {"sundays", "SU"},
+            {"monday", "MO"},
+            {"tuesday", "TU"},
+            {"wednesday", "WE"},
+            {"thursday", "TH"},
+            {"friday", "FR"},
+            {"saturday", "SA"},
+            {"sunday", "SU"},
+            {"mon", "MO"},
+            {"tue", "TU"},
+            {"wed", "WE"},
+            {"thu", "TH"},
+            {"fri", "FR"},
+            {"sat", "SA"},
+            {"sun", "SU"},
+            {" - ", "-"},
+            {" midday ", "12:00"},
+            {" noon ", "12:00"},
+            {" midnight ", "00:00"}
+        };
+
         /// <summary>
         /// Generates schedule information from Days and Frequency
         /// </summary>
@@ -14,73 +48,56 @@ namespace ServiceLoader
         /// <returns>Schedule information which is not guaranteed to be completely accurate</returns>
         public static IEnumerable<Schedule> Build(Result result)
         {
-            var validDays = new string[] { "SU", "MO", "TU", "WE", "TH", "FR", "SA" };
+            if (string.IsNullOrEmpty(result.Frequency) && !result.Days.Any()) yield break;
+
             var text = result.Frequency?.ToLowerInvariant() ?? string.Empty;
-            text = text.Replace("&", " and ")
-                       .Replace(",", " and ")
-                       .Replace("mondays", "MO")
-                       .Replace("tuesdays", "TU")
-                       .Replace("wednesdays", "WE")
-                       .Replace("thursdays", "TH")
-                       .Replace("fridays", "FR")
-                       .Replace("saturdays", "SA")
-                       .Replace("sundays", "SU")
-                       .Replace("monday", "MO")
-                       .Replace("tuesday", "TU")
-                       .Replace("wednesday", "WE")
-                       .Replace("thursday", "TH")
-                       .Replace("friday", "FR")
-                       .Replace("saturday", "SA")
-                       .Replace("sunday", "SU")
-                       .Replace("mon", "MO")
-                       .Replace("tue", "TU")
-                       .Replace("wed", "WE")
-                       .Replace("thu", "TH")
-                       .Replace("fri", "FR")
-                       .Replace("sat", "SA")
-                       .Replace("sun", "SU")
-                       .Replace(" - ", "-")
-                       .Replace(" midday ", "12:00")
-                       .Replace(" noon ", "12:00")
-                       .Replace(" midnight ", "00:00");
-
-            var parts = text.Split(new[] {"and", " "}, StringSplitOptions.RemoveEmptyEntries).Select(part => part.Trim());
-            var weeklyMonthlyWords = new string[]{"alternate", "1st", "2nd", "3rd", "4th", "first", "second", "third", "fourth", "last"};
-            var weeklyMonthly = parts.Intersect(weeklyMonthlyWords, StringComparer.OrdinalIgnoreCase).Any() ? "MONTHLY" : "WEEKLY";
-            var days = result.Days.Distinct().ToList();
-            var times = new List<Times>();
-
-            foreach (var part in parts)
+            foreach (var wordReplacement in WordReplacements)
             {
-                if (validDays.Contains(part, StringComparer.OrdinalIgnoreCase))
-                {
-                    if (!days.Contains(part, StringComparer.OrdinalIgnoreCase)) days.Add(part);
-                    continue;
-                }
-
-                if (char.IsDigit(part, 0))
-                {
-                    var timeParts = part.Split('-').Select(timePart => timePart.Trim()).ToArray();
-                    if (timeParts.Length > 2) continue;
-                    if (!TimeSpan.TryParse(timeParts[0], out var startTime)) continue;
-                    var time = new Times{StartTime = startTime};
-                    if (timeParts.Length > 1 && TimeSpan.TryParse(timeParts[1], out var endTime)) time.EndTime = endTime;
-                    times.Add(time);
-                }
+                text = text.Replace(wordReplacement.Key, wordReplacement.Value);
             }
 
-            if (times.Count > 1) times.Clear(); //better to write no information than partial information - we are targetting the simple records
+            var parts = text.Split(new[] {"and", " "}, StringSplitOptions.RemoveEmptyEntries).Select(part => part.Trim());
+            var weeklyMonthly = parts.Intersect(MonthlyWords, StringComparer.OrdinalIgnoreCase).Any() ? "MONTHLY" : "WEEKLY";
+            var times = ParseTimes(parts);
+            var days = result.Days.Any() ? result.Days.Distinct() : ParseDays(parts);
             
             foreach (var day in days)
             {
                 if (day.Length < 2) continue;
                 var dayAbbrv = day.Substring(0, 2).ToUpperInvariant();
-                if (!validDays.Contains(dayAbbrv, StringComparer.Ordinal)) continue;
+                if (!ValidDays.Contains(dayAbbrv, StringComparer.Ordinal)) continue;
 
-                yield return new Schedule($"{result.ServiceId}:{day}", dayAbbrv, result.Frequency, weeklyMonthly, times.FirstOrDefault()?.StartTime, times.FirstOrDefault()?.EndTime);
+                yield return new Schedule($"{result.ServiceId}:{day}", dayAbbrv, result.Frequency, weeklyMonthly, times?.StartTime, times?.EndTime);
             }
 
-            if (!days.Any()) yield return new Schedule($"{result.ServiceId}:unknown", string.Empty, result.Frequency, weeklyMonthly, times.FirstOrDefault()?.StartTime, times.FirstOrDefault()?.EndTime);
+            if (!days.Any()) yield return new Schedule($"{result.ServiceId}:unknown", string.Empty, result.Frequency, weeklyMonthly, times?.StartTime, times?.EndTime);
+        }
+
+        private static IEnumerable<string> ParseDays(IEnumerable<string> parts)
+        {
+            return parts.Where(part => ValidDays.Contains(part, StringComparer.OrdinalIgnoreCase)).Distinct();
+        }
+
+        private static Times ParseTimes(IEnumerable<string> parts)
+        {
+            var times = new List<Times>();
+            var formats = new string[]{ "hh\\:mm", "h\\:mm" };
+
+            foreach (var part in parts)
+            {
+                if (char.IsDigit(part, 0))
+                {
+                    var timeParts = part.Split('-').Select(timePart => timePart.Trim()).ToArray();
+                    if (timeParts.Length > 2) continue;
+                    if (!TimeSpan.TryParseExact(timeParts[0], formats, null, out var startTime)) continue;
+                    var time = new Times { StartTime = startTime };
+                    if (timeParts.Length > 1 && TimeSpan.TryParseExact(timeParts[1], formats, null, out var endTime)) time.EndTime = endTime;
+                    times.Add(time);
+                }
+            }
+
+            if (times.Count > 1) times.Clear(); //better to write no information than partial information - we are targetting the simple records
+            return times.FirstOrDefault();
         }
 
         private class Times
