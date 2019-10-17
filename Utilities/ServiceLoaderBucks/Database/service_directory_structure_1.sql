02643ea4b74e1c2d7d6978836cc399f73506df88
esd-org-uk <support@esd.org.uk>
2019-10-17 14:58:04 +0100

    Add new fields to DB
    These are fields which were assigned MUST or SHOULD status in the application profile

---
+++ Utilities/ServiceLoaderBucks/Database/service_directory_structure_1.sql
@@ -0,0 +1,29 @@
+ALTER TABLE organization ADD email text;
+ALTER TABLE organization ADD legal_status text;
+ALTER TABLE organization ADD assured_date datetime;
+
+ALTER TABLE service ADD application_process text;
+ALTER TABLE service ADD assired_date datetime;
+
+ALTER TABLE eligibility ADD minimum_age int;
+ALTER TABLE eligibility ADD maximum_age int;
+
+ALTER TABLE service_taxonomy ADD eligibility_id varchar(1536);
+
+ALTER TABLE contact ADD email text;
+ALTER TABLE contact ADD personal_data boolean;
+ALTER TABLE contact ADD sensitive_data boolean;
+
+CREATE TABLE `identifier` (
+	id varchar(1536) NOT NULL,
+	organization_id varchar(1536),
+	service_area_id varchar(1536),
+	register text,
+	identifier text,
+	uri text,
+	PRIMARY KEY(id),
+	KEY FK_identifier_organization (organization_id),
+	KEY FK_identifier_service_area (service_area_id),
+	CONSTRAINT FK_identifier_organization FOREIGN KEY (organization_id) REFERENCES organization (id),
+	CONSTRAINT FK_identifier_service_area FOREIGN KEY (service_area_id) REFERENCES service_area (id)
+);
