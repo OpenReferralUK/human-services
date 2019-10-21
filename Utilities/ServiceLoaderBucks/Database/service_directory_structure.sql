93b9907e349beb64022501bd664750f29489b3e2
esd-org-uk <support@esd.org.uk>
2019-10-21 09:20:44 +0100

    Merge sql scripts
    There were two sql scripts to run one after the other to create the initial empty database. These have been merged into a single script.

--- Utilities/ServiceLoaderBucks/Database/service_directory_structure.sql
+++ Utilities/ServiceLoaderBucks/Database/service_directory_structure.sql
@@ -419,3 +419,46 @@ CREATE TABLE `taxonomy` (
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+/*Additional fields and table from service_directory_structure_1.sql, which can now be deleted*/
+
+#MUST
+ALTER TABLE organization ADD email text;
+ALTER TABLE organization ADD legal_status text;
+ALTER TABLE organization ADD assured_date datetime;
+
+ALTER TABLE service ADD application_process text;
+ALTER TABLE service ADD assured_date datetime;
+
+ALTER TABLE eligibility ADD minimum_age int;
+ALTER TABLE eligibility ADD maximum_age int;
+
+ALTER TABLE service_taxonomy ADD eligibility_id varchar(1536);
+
+ALTER TABLE contact ADD email text;
+ALTER TABLE contact ADD personal_data boolean;
+ALTER TABLE contact ADD sensitive_data boolean;
+
+DROP TABLE IF EXISTS `identifier`;
+CREATE TABLE `identifier` (
+	id varchar(1536) NOT NULL,
+	organization_id varchar(1536),
+	service_area_id varchar(1536),
+	register text,
+	identifier text,
+	uri text,
+	PRIMARY KEY(id),
+	KEY FK_identifier_organization (organization_id),
+	KEY FK_identifier_service_area (service_area_id),
+	CONSTRAINT FK_identifier_organization FOREIGN KEY (organization_id) REFERENCES organization (id),
+	CONSTRAINT FK_identifier_service_area FOREIGN KEY (service_area_id) REFERENCES service_area (id)
+);
+
+#SHOULD
+ALTER TABLE physical_address
+ADD uprn text;
+
+ALTER TABLE contact ADD department text;
+
+ALTER TABLE phone ADD extension text;
+ALTER TABLE phone ADD description text;
