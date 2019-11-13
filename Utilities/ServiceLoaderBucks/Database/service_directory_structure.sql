f0ca5bff1aa400e051b94e7680b1757a6d836715
esd-org-uk <support@esd.org.uk>
2019-11-13 09:41:33 +0000

    Add table for external ids
    New table to store UUIDs and the id from the source data

--- Utilities/ServiceLoaderBucks/Database/service_directory_structure.sql
+++ Utilities/ServiceLoaderBucks/Database/service_directory_structure.sql
@@ -483,3 +483,11 @@ ALTER TABLE contact ADD department text;
 
 ALTER TABLE phone ADD extension text;
 ALTER TABLE phone ADD description text;
+
+CREATE TABLE `esd_external_id` (
+  `uuid` varchar(1586) NOT NULL,
+  `external_id` varchar(1586) DEFAULT NULL,
+  `origin` text,
+  `reference_table` text,
+  PRIMARY KEY (`uuid`)
+);
