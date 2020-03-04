eedb80c0b0b0a37b813de3e9c9055475ff56bee6
PorismJamesDavidson <James.Davidson@porism.com>
2020-03-04 15:49:41 +0000

    Remove ServiceFinder

--- ServiceFinder/src/Store/reducers/tempSearches.jsx
+++
@@ -1,9 +0,0 @@
-const tempSearches = function (state = false, action) {
-    switch (action.type) {
-        case 'SET_HOME':
-            return (state = true);
-        default: return state;
-    }
-}
-
-export default tempSearches;
