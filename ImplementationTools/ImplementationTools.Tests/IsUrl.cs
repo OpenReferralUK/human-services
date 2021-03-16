777c840eeaa1fc8b5da21f37e6d2bbcd30f7f42b
yemijaiyeola <yemi.jaiyeola@wearesnook.com>
2021-03-16 10:49:35 +0000

    add tests project

---
+++ ImplementationTools/ImplementationTools.Tests/IsUrl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xunit;
+using ServiceDirectoryExporter.Pages;
+using ServiceDirectory.Common;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Net.Http;
+using Moq.Protected;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImplementationTools.Tests
+{
+    
+    public class IsUrl
+    {
+
+        [Theory]
+        [InlineData("/path")]
+        [InlineData("ftp://www.something.com")]
+        public void isURLStringValid(string value)
+        {
+            var logger = new Mock<ILogger>();
+
+            Urls url = new Urls(value);
+            
+            bool result = url.urlValid();
+            Assert.False(result);
+
+        }
+
+        [Theory]
+        [InlineData("https://outpost-api-service.herokuapp.com/api/v1/")]
+        [InlineData("https://api.porism.com/ServiceDirectoryServiceBlackburn/")]
+        [InlineData("https://lgaapi.connecttosupport.org/")]
+        public void isURLStringValidTrue(string value)
+        {
+     
+            var logger = new Mock<ILogger>();
+            Urls url = new Urls(value);
+            bool result =url.urlValid();
+            Assert.True(result);
+
+        }
+       
+    }
+}
---
+++ ImplementationTools/ServiceDirectory.Common/Urls.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace ServiceDirectory.Common
+{
+    public  class Urls
+    {
+        private readonly string url;
+
+       public Urls(string url)
+        {
+            this.url = url;
+        }
+
+        public  bool urlValid()
+        {
+            Regex urlPattern = new Regex(@"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$");
+            Match matches = urlPattern.Match(url);
+            return matches.Success;
+        }
+
+       
+    }
+}
