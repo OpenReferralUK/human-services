9a8f2a981e4a5f787b2f5609771526a55302ac85
PorismDominicSkinner <54143283+PorismDominicSkinner@users.noreply.github.com>
2021-08-26 17:08:15 +0100

    Fix for too many response error

--- ImplementationTools/ServiceDirectory.Common/WebServiceReader.cs
+++ ImplementationTools/ServiceDirectory.Common/WebServiceReader.cs
@@ -16,16 +16,10 @@ namespace ServiceDirectory.Common
         }
         internal static async System.Threading.Tasks.Task<dynamic> ConvertToDynamic(string url, int attempt = 0)
         {
-            var response = await client.GetAsync(url);
-            byte[] result = await response.Content.ReadAsByteArrayAsync();
-            string s = Encoding.UTF8.GetString(result);
             try
             {
-                return Newtonsoft.Json.JsonConvert.DeserializeObject(s);
-            }
-            catch(Exception e)
-            {
-                if (response != null && response.StatusCode == (System.Net.HttpStatusCode)429)
+                var response = await client.GetAsync(url);
+                if (response.StatusCode == (System.Net.HttpStatusCode)429)
                 {
                     attempt++;
                     if (attempt < 3)
@@ -33,7 +27,14 @@ namespace ServiceDirectory.Common
                         await Task.Delay(60000);
                         return await ConvertToDynamic(url, attempt).ConfigureAwait(false);
                     }
+                    return null;
                 }
+                byte[] result = await response.Content.ReadAsByteArrayAsync();
+                string s = Encoding.UTF8.GetString(result);
+                return Newtonsoft.Json.JsonConvert.DeserializeObject(s);
+            }
+            catch(Exception e)
+            {
                 return null;
             }
         }
